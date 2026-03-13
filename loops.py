75d30c4dba2d22c9bae4bb14c6f0e2443289605b
Tanmay Itape <itapetanmay103@gmail.com>
2026-03-13 13:25:02 +0530

    3 new problems from HackerRank

---
+++ loops.py
@@ -0,0 +1,41 @@
+"""
+Task
+The provided code stub reads an integer, , from STDIN. For all non-negative integers , print .
+
+Example
+
+The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+0
+1
+4
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    
+for i in range(i,n):
+    print(i*i)
+    i+=1
